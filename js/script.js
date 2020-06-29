55e444544a8575d976a020610bb8268013db8fcd
Bartosz Bodziony <bartosz1293@gmail.com>
2020-06-29 16:17:55 +0200

    day 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("");
+
+console.log("czesc")
+
+
+let formElement = document.querySelector (".js-form");
+let amountElement = document.querySelector (".js-amount");
+let currencyElement = document.querySelector (".js-currency");
+let resultElement = document.querySelector (".js-result");
+
+let EUR = 4.431;
+let GBP = 4.499;
+let USD = 4.210;
+
+
+
+
+form.addEventListener("submit") , (event) => {event.preventDefault();
